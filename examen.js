b38c58db0cf9fc0fc125ca7ab2c1ac80ca081922
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2023-09-15 08:40:12 -0300

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,30 @@
+//Practica 1
+let salarioAnual;(x / x)
+{
+    let resultado = (x / x)
+    return resultado
+}
+console.log (calcular, salarioAnual(x/x))
+
+
+//Practica 2
+let salarioMensual;(x * x)
+{
+    let resultado = (x * x)
+    return resultado
+}
+console.log (calcular, salarioMensual(x*x))
+
+
+//Practica 3
+let promedioNota;(Nota1,Nota2,Nota3)
+{
+    let promedio = (Nota1 + Nota2 + Nota3)
+    promedio = promedio / 3
+    return promedio 
+
+    alert ('Tu nota es' + (Nota1, Nota2, Nota3)) 
+
+}
+result = prompt (promedioNota + 'tu nota es' )
+
