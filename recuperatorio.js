08bf239e65aeeb2e3a096c10eca12383f5f804f2
alumno <alumno@Instituto.local>
2023-09-22 09:28:31 -0300

    agregar archivos

---
+++ recuperatorio.js
@@ -0,0 +1,35 @@
+function calculoIvaProducto (iva)
+{
+    calcular (21 = 21 * x)
+    return  x / 100
+}
+console.log (iva(21/x)= x/100)
+
+
+function descuentoIva (x)
+{
+    calcular (a/b)
+    return resultado
+}
+console.log (descuentoIva(a/b))
+
+
+
+
+function conviertoDolarAPeso (x)
+{
+    calcular = (740 * x)
+    return resultado
+}
+console.log (conviertoDolarAPeso(740*x))
+calcular = number(prompt(conviertoDolarAPeso))
+
+function conviertoPesoADolar (x)
+{
+    calcular = (x / x)
+    return resultado
+}
+console.log (conviertoDolarAPeso(x / x))
+calcular = number(prompt(conviertoPesoADolar))
+
+
